1809aa582e737bc2b37ca3a5ed3279daa37b74a6
Konstantin <myvvot@gmail.com>
2026-06-09 16:18:02 +0300

    Update auth_api.go

--- internal/auth/api/auth_api.go
+++ internal/auth/api/auth_api.go
@@ -1,9 +1,10 @@
 package api
 
 import (
+	"context"
+
 	"AgentManagmentSystem/internal/auth/service"
 	server "AgentManagmentSystem/pkg/api/grpc/auth/v1"
-	"context"
 )
 
 type AuthServiceApi struct {
@@ -11,22 +12,24 @@ type AuthServiceApi struct {
 	authService *service.AuthService
 }
 
-func (a AuthServiceApi) Login(ctx context.Context, request *server.LoginRequest) (*server.LoginResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func NewAuthServiceApi(authService *service.AuthService) *AuthServiceApi {
+	return &AuthServiceApi{
+		authService: authService,
+	}
 }
 
-func (a AuthServiceApi) RefreshToken(ctx context.Context, request *server.RefreshTokenRequest) (*server.RefreshTokenResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (a *AuthServiceApi) Login(ctx context.Context, request *server.LoginRequest) (*server.LoginResponse, error) {
+	return a.authService.Login(ctx, request)
 }
 
-func (a AuthServiceApi) Logout(ctx context.Context, request *server.LogoutRequest) (*server.LogoutResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (a *AuthServiceApi) RefreshToken(ctx context.Context, request *server.RefreshTokenRequest) (*server.RefreshTokenResponse, error) {
+	return a.authService.RefreshToken(ctx, request)
 }
 
-func (a AuthServiceApi) CheckPermission(ctx context.Context, request *server.CheckPermissionRequest) (*server.CheckPermissionResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (a *AuthServiceApi) Logout(ctx context.Context, request *server.LogoutRequest) (*server.LogoutResponse, error) {
+	return a.authService.Logout(ctx, request)
+}
+
+func (a *AuthServiceApi) CheckPermission(ctx context.Context, request *server.CheckPermissionRequest) (*server.CheckPermissionResponse, error) {
+	return a.authService.CheckPermission(ctx, request)
 }
