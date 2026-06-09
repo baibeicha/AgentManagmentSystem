4b0909757f2baf6bd653845af5704be1b3ffaf48
Konstantin <myvvot@gmail.com>
2026-06-09 15:37:21 +0300

    Created DB struct using pgx

---
+++ pkg/storage/db.go
@@ -0,0 +1,62 @@
+package storage
+
+import (
+	"AgentManagmentSystem/pkg/config"
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type DB struct {
+	*pgxpool.Pool
+	datasource string
+}
+
+func (db *DB) RunMigrations() error {
+	return RunMigrations(db.Pool)
+}
+
+func Connect(url, dbname, user, password, sslmode string) (*DB, error) {
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+
+	datasource := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s", user, password, url, dbname, sslmode)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, datasource)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create connection pool: %w", err)
+	}
+
+	return &DB{
+		Pool:       pool,
+		datasource: datasource,
+	}, nil
+}
+
+func ConnectSource(source *config.Datasource) (*DB, error) {
+	db, err := Connect(
+		source.Url,
+		source.DB,
+		source.User,
+		source.Password,
+		source.SslMode,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to datasource: %w", err)
+	}
+
+	return db, nil
+}
+
+func (db *DB) Close() {
+	db.Pool.Close()
+	slog.Info("database connection pool closed successfully")
+}
---
+++ pkg/storage/migration.go
@@ -0,0 +1,51 @@
+package storage
+
+import (
+	"embed"
+	"errors"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+)
+
+//go:embed ../../migrations/*.sql
+var migrationsFS embed.FS
+
+func RunMigrations(db *pgxpool.Pool) error {
+	sqlDB := stdlib.OpenDB(*db.Config().ConnConfig)
+	defer sqlDB.Close()
+
+	dbDriver, err := pgx.WithInstance(sqlDB, &pgx.Config{})
+	if err != nil {
+		return fmt.Errorf("error creating database driver: %w", err)
+	}
+
+	sourceDriver, err := iofs.New(migrationsFS, "migrations")
+	if err != nil {
+		return fmt.Errorf("error creating embed migration files: %w", err)
+	}
+
+	m, err := migrate.NewWithInstance(
+		"iofs",
+		sourceDriver,
+		"pgx5",
+		dbDriver,
+	)
+	if err != nil {
+		return fmt.Errorf("error initializing migrate: %w", err)
+	}
+
+	err = m.Up()
+	if err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			return nil
+		}
+		return fmt.Errorf("error applying migrations: %w", err)
+	}
+
+	return nil
+}
